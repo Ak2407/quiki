61a037cc35c3f5c2e921532925ca0148b5276cc5
Akshit Gupta <akshu.24gupta@gmail.com>
2024-10-22 10:51:29 +0530

    library done

---
+++ src/app/dashboard/library/_components/SearchBar.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import * as React from "react";
+import {
+  CalendarIcon,
+  EnvelopeClosedIcon,
+  FaceIcon,
+  GearIcon,
+  PersonIcon,
+  RocketIcon,
+} from "@radix-ui/react-icons";
+
+import {
+  CommandDialog,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+  CommandSeparator,
+  CommandShortcut,
+} from "@/components/ui/command";
+import { Button } from "@/components/ui/button";
+
+export function SearchBar() {
+  const [open, setOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if (e.key === "k" && (e.metaKey || e.ctrlKey)) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener("keydown", down);
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  return (
+    <>
+      <Button
+        variant="outline"
+        className="w-[80%] sm:w-[300px] rounded-lg "
+        onClick={() => setOpen(true)}
+      >
+        <span className="flex w-full items-center justify-between text-sm text-neutral-600 ">
+          <p>Search for your video... </p>
+
+          <kbd className="hidden sm:inline-flex pointer-events-none h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground opacity-100">
+            <span className="text-xs">⌘</span>K
+          </kbd>
+        </span>
+      </Button>
+      <CommandDialog open={open} onOpenChange={setOpen}>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>
+              <CalendarIcon />
+              <span>Calendar</span>
+            </CommandItem>
+            <CommandItem>
+              <FaceIcon />
+              <span>Search Emoji</span>
+            </CommandItem>
+            <CommandItem>
+              <RocketIcon />
+              <span>Launch</span>
+            </CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem>
+              <PersonIcon />
+              <span>Profile</span>
+              <CommandShortcut>⌘P</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <EnvelopeClosedIcon />
+              <span>Mail</span>
+              <CommandShortcut>⌘B</CommandShortcut>
+            </CommandItem>
+            <CommandItem>
+              <GearIcon />
+              <span>Settings</span>
+              <CommandShortcut>⌘S</CommandShortcut>
+            </CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </CommandDialog>
+    </>
+  );
+}
--- src/app/dashboard/library/page.tsx
+++ src/app/dashboard/library/page.tsx
@@ -1,5 +1,119 @@
+import GeneratedVid from "../_components/GeneratedVid";
+import HeaderBar from "../_components/HeaderBar";
+import { SearchBar } from "./_components/SearchBar";
+
 const Library = () => {
-  return <div>Library</div>;
+  return (
+    <div className="w-full pt-20 sm:pt-8 p-8 flex flex-col gap-6">
+      <div className="w-full flex justify-center sm:justify-end items-center fixed top-0 right-0 p-4">
+        <SearchBar />
+      </div>
+      <HeaderBar title="Library" />
+      <div className="flex flex-wrap gap-6 w-full ">
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+        <GeneratedVid />
+      </div>
+    </div>
+  );
 };
 
 export default Library;
