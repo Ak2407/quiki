2e2934fe2d5dcc8cc7d469648fa0a4aab7f49158
Akshit Gupta <akshu.24gupta@gmail.com>
2024-10-24 20:50:12 +0530

    select button

--- src/app/create/_components/Topic.tsx
+++ src/app/create/_components/Topic.tsx
@@ -1,5 +1,31 @@
 const Topic = () => {
-  return <div>Topic</div>;
+  return (
+    <div className="flex flex-row flex-wrap items-center lg:grid grid-cols-5 gap-4 overflow-auto h-full">
+      <SelectButton text="Scary Stories" emoji="😱" />
+      <SelectButton text="Motivational" emoji="💪" />
+      <SelectButton text="Educational" emoji="📚" />
+      <SelectButton text="Bedtime Stories" emoji="😴" />
+      <SelectButton text="Love Stories" emoji="💞" />
+      <SelectButton text="Interesting History" emoji="📜" />
+      <SelectButton text="Fun Facts" emoji="🤔" />
+      <SelectButton text="Mystery" emoji="🕵️‍♂️" />
+      <SelectButton text="Life Pro Tips" emoji="💡" />
+      <SelectButton text="Long Form Jokes" emoji="😂" />
+      <SelectButton text="Travel Stories" emoji="✈️" />
+      <SelectButton text="Philosophy" emoji="🤔" />
+      <SelectButton text="Science Fiction" emoji="👽" />
+      <SelectButton text="Custom" emoji="✍️" />
+    </div>
+  );
+};
+
+const SelectButton = ({ text, emoji }: { text: string; emoji: string }) => {
+  return (
+    <div className="flex flex-col gap-2 items-center justify-center p-5 lg:w-40 w-full h-40 bg-gray-50 border rounded-lg cursor-pointer hover:bg-gray-100 transition-all ease-in-out duration-200 text-center ">
+      <h1 className="font-semibold">{text}</h1>
+      <p className="text-4xl">{emoji}</p>
+    </div>
+  );
 };
 
 export default Topic;
--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -8,16 +8,26 @@ import Language from "./_components/Language";
 import Voice from "./_components/Voice";
 import Subtitle from "./_components/Subtitle";
 import Review from "./_components/Review";
+import { steps } from "@/lib/constants";
 
 const CreatePage = () => {
   const [step, setStep] = useState<number>(0);
 
   return (
-    <div className="min-h-screen pt-10 flex flex-col gap-2 items-center justify-center">
+    <div className="min-h-screen p-10 pb-40 flex flex-col gap-2 items-center justify-center">
       <div className="w-full fixed top-0 left-0 p-2 ">
         <CancelButton />
       </div>
-      {step === 0 && <Topic />}
+      {step === 0 && (
+        <div className="flex flex-col gap-10">
+          <h1 className="text-2xl font-bold text-center">
+            {" "}
+            {steps[step].title}
+          </h1>
+
+          <Topic />
+        </div>
+      )}
       {step === 1 && <Language />}
       {step === 2 && <Voice />}
       {step === 3 && <Subtitle />}
