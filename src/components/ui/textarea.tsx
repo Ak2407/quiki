06ff2d1f582616109832da8247158c24fc02b3e6
Akshit Gupta <akshu.24gupta@gmail.com>
2024-10-30 22:24:43 +0530

    textarea size max

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -9,7 +9,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-[60px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground outline-none focus:border-neutral-300 transition-all ease-in-out duration-200  disabled:cursor-not-allowed disabled:opacity-50",
+          "flex min-h-[60px] max-h-[250px] w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm shadow-sm placeholder:text-muted-foreground outline-none focus:border-neutral-300 transition-colors ease-in-out duration-200  disabled:cursor-not-allowed disabled:opacity-50",
           className,
         )}
         ref={ref}
