f45b3b5539aae5cbbab1c42375c2ad91103d2915
Akshit Gupta <akshu.24gupta@gmail.com>
2024-11-07 21:37:19 +0530

    Alert loading

--- src/app/dashboard/[id]/_components/UpgradeAlert.tsx
+++ src/app/dashboard/[id]/_components/UpgradeAlert.tsx
@@ -1,18 +1,34 @@
+"use client";
+
 import { Alert, AlertDescription } from "@/components/ui/alert";
-import Link from "next/link";
+import { LoaderIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const UpgradeAlert = () => {
+  const router = useRouter();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleUpgrade = () => {
+    setLoading(true);
+    router.push("/dashboard/billing");
+  };
+
   return (
     <Alert className="mb-6 bg-red-50 border-red-200 text-red-900">
       <AlertDescription className="flex gap-2 items-center justify-between">
         <span>
           Cannot generate more videos on the free plan. Upgrade to create more.
         </span>
-        <Link href="/dashboard/billing">
-          <h1 className="text-sm font-medium hover:underline underline-offset-4">
-            Upgrade
-          </h1>
-        </Link>
+        <button onClick={handleUpgrade} disabled={loading}>
+          {loading ? (
+            <LoaderIcon className="animate-spin h-5 w-5 mr-2" />
+          ) : (
+            <h1 className="text-sm font-medium hover:underline underline-offset-4">
+              Upgrade
+            </h1>
+          )}
+        </button>
       </AlertDescription>
     </Alert>
   );
