3b08aeda8303533fc77dd62dbbf660e8b6052930
Akshit Gupta <akshu.24gupta@gmail.com>
2024-11-07 21:18:35 +0530

    Back to Library button on update form

--- src/app/dashboard/[id]/_components/VideoHeader.tsx
+++ src/app/dashboard/[id]/_components/VideoHeader.tsx
@@ -1,11 +1,18 @@
-import TitleForm from "./TitleForm";
+import Link from "next/link";
 import DeleteVideoBtn from "./DeleteVideoBtn";
+import { ChevronLeftIcon } from "lucide-react";
 
 const VideoHeader = () => {
   return (
     <div className="flex items-center justify-between border-b pb-4">
       <div className="flex items-center gap-4">
-        <TitleForm />
+        <Link
+          href="/dashboard/library"
+          className="flex items-center gap-2 text-gray-500 hover:text-gray-700"
+        >
+          <ChevronLeftIcon className="h-5 w-5 " />
+          <span className="">Back to Library</span>
+        </Link>
       </div>
       <div className="flex gap-2">
         <DeleteVideoBtn />
